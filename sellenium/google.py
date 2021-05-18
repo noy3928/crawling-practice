98aff3e7d6cfb73b974269027ca5b488af16d535
noy3928@naver.com <noyechan@noyechan-ui-MacBook-Pro.local>
2021-05-18 23:44:03 +0900

    xpath upadate

--- sellenium/google.py
+++ sellenium/google.py
@@ -8,12 +8,21 @@ import urllib.request
 
 driver = webdriver.Chrome(executable_path = r'/Users/noyechan/Desktop/파이썬/selenium/sellenium/chromedriver')
 driver.get("https://www.weather.go.kr/w/index.do")
+
+
 tmp = driver.find_element_by_css_selector(".tmp").text
 hue = driver.find_element_by_css_selector("span.val").text
 wind = driver.find_elements_by_css_selector("span.val")
+tmp1 = driver.find_elements_by_css_selector("g.highcharts-label")
+feelTmp = driver.find_element_by_xpath("//ul[@class='item s-item  ']/li[2]/span[2]")
+
 print(tmp)
 print(hue)
 print(wind[1].text)
+print(tmp1[85])
+print(feelTmp.text)
+# for i in range(len(tmp1)):
+#     print(tmp1[i].text)
 
 
 
