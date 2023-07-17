4e005a5480c1213047f277dbd2b2af90c0ed3dc4
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-07-17 18:01:38 +0900

    스크롤 내릴때 함수 수정. 무한스크롤시 네트워크 로드 걸리는 부분 시간 충분히 가지도록 수정

--- wanted/utils/scrollDown.js
+++ wanted/utils/scrollDown.js
@@ -1,6 +1,7 @@
 const scrollDown = async page => {
   let prevNumElements = 0
   let scrollCount = 0
+  let noChangeCount = 0 // 추가: 변화가 없는 경우를 세기 위한 카운터
 
   // 스크롤 시작
   while (true) {
@@ -14,8 +15,14 @@ const scrollDown = async page => {
       const elements = await page.$$("[data-cy='job-card']")
       console.log(elements.length, prevNumElements)
       if (prevNumElements === elements.length) {
-        console.log("스크롤 종료!")
-        break
+        noChangeCount++ // 추가: 변화가 없는 경우 카운터 증가
+        if (noChangeCount >= 5) {
+          // 추가: 변화가 5번 이상 없는 경우에만 스크롤 종료
+          console.log("스크롤 종료!")
+          break
+        }
+      } else {
+        noChangeCount = 0 // 추가: 요소의 수가 변화한 경우 카운터 초기화
       }
       prevNumElements = elements.length
     }
